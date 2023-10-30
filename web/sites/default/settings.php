c172a48434dfc2dc384c158b7161fa74ec28272b
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2023-10-30 15:57:19 -0600

    Inicio de rama dev

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -265,7 +265,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'GGFyBpyhvJUE73MDecKMYVIv4x16SWOCRIjkv1R7_xXubKTQBWEwdBQBTgHG29nj5OxG5eiD5w';
+$settings['hash_salt'] = '0gjwcIU-eO6OD6vmO4pPtyMJjZLNDk_zxvE7KSxUpzNki35XV7asciIutnDuwOxc3jk2JhyTaA';
 
 /**
  * Deployment identifier.
@@ -857,19 +857,19 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
- if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-   include $app_root . '/' . $site_path . '/settings.local.php';
- }
+# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+#   include $app_root . '/' . $site_path . '/settings.local.php';
+# }
 $databases['default']['default'] = array (
-  'database' => 'plantilla_web',
-  'username' => 'plantilla_web',
-  'password' => 'plantilla_web',
+  'database' => 'webPlantillaWSYS',
+  'username' => 'webPlantillaWSYS',
+  'password' => 'webPlantillaWSYS',
   'prefix' => '',
-  'host' => '127.0.0.1',
+  'host' => 'localhost',
   'port' => '3306',
   'isolation_level' => 'READ COMMITTED',
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'driver' => 'mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
-$settings['config_sync_directory'] = 'sites/default/files/config_lvUdobG4IXKVzM7285sQPpF1pdTSkspM9CEnU9xifIU7hleZ-e-LLfkBwwg1ZboP2rsc79mIUg/sync';
+$settings['config_sync_directory'] = 'sites/default/files/config_El-gMwRMr6VamjmCsJQ4FWYWe4i9zqqA8G5PaIatBOkoRYiBsjDgui_omrFWmSjeZNYUWs519A/sync';
