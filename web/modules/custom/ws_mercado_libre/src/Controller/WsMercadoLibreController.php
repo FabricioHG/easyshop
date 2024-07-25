dd8b51e8ebae9ebce3372c2cd0a2de175e806af2
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2024-07-24 18:14:32 -0600

    Se hizo la programacion para conectar con Mercado libre, para obtener el token y guardarlo al usuario

---
+++ web/modules/custom/ws_mercado_libre/src/Controller/WsMercadoLibreController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Drupal\ws_mercado_libre\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\ws_mercado_libre\Form\MercadoLibreProducts;
+use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
+//use Symfony\Component\HttpFoundation\Response;
+use \Drupal\Core\Config\ConfigFactoryInterface;
+
+
+
+
+/**
+ * Returns responses for Ws mercado libre routes.
+ */
+final class WsMercadoLibreController extends ControllerBase {
+
+  protected $formBuilder;
+  protected $configFactory;
+
+  public function __construct(FormBuilderInterface $form_builder, ConfigFactoryInterface $config_factory) {
+    $this->formBuilder = $form_builder;
+    $this->configFactory = $config_factory;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('form_builder'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function page_config(UserInterface $user) {
+
+     $form = $this->formBuilder->getForm('Drupal\ws_mercado_libre\Form\MercadoLibreProducts', $user);
+
+    $build = [
+      'formulario' => $form,
+    ];
+
+
+    return $build;
+  }
+
+  public function notify(Request $request) {
+    $user = \Drupal::currentUser();
+
+    $auth_code = $request->query->get('SERVER_GENERATED_AUTHORIZATION_CODE');
+
+    if (!$auth_code) {
+      \Drupal::messenger()->addError($this->t('Failed to connect to Mercado Libre.'));
+      \Drupal::logger('ws_mercado_libre')->notice('Failed to connect to Mercado Libre to user. %user', ['%user' => $user->getAccountName()]);
+      return $this->redirect('ws_mercado_libre.user', ['user' => $user->id()]);
+      
+    }
+   
+    $config = $this->configFactory->get('ws_mercado_libre.settings');
+    $client_id = $config->get('client_id');
+    $client_secret = $config->get('client_secret');
+    $redirect_uri = $config->get('url_redirect');
+
+    $client = new Client();
+    $response = $client->post('https://api.mercadolibre.com/oauth/token', [
+      'form_params' => [
+        'grant_type' => 'authorization_code',
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'code' => $auth_code,
+        'redirect_uri' => $redirect_uri,
+      ],
+    ]);
+   
+  if ($response->getStatusCode() == 200) {
+    $data = json_decode($response->getBody(), true);
+    $access_token = $data['access_token'];
+    $refresh_token = $data['refresh_token'];
+
+    // Save the tokens to the user's configuration or database.
+    $user = \Drupal::currentUser();
+    $account = \Drupal\user\Entity\User::load($user->id());
+    $account->set('field_mercadolibre_access_token', $access_token);
+    $account->set('field_mercadolibre_refresh_token', $refresh_token);
+    $account->save(); 
+
+    \Drupal::messenger()->addMessage($this->t('Successfully connected to Mercado Libre.'));
+    return $this->redirect('ws_mercado_libre.user', ['user' => $user->id()]); 
+  }
+  else {
+      \Drupal::messenger()->addError($this->t('Failed to connect to Mercado Libre.'));
+    }
+
+  return $this->redirect('ws_mercado_libre.user', ['user' => $user->id()]);
+    
+  }//Fin de notify
+
+
+}//Fin del controlador
+
+
+
+
+
+
+
+
+
+
---
+++ web/modules/custom/ws_mercado_libre/src/Form/MercadoLibreProducts.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ws_mercado_libre\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use \Drupal\Core\Config\ConfigFactoryInterface;
+use \Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Provides a WS Mercado Libre form.
+ */
+final class MercadoLibreProducts extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  protected $configFactory;
+
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
+
+  public function getFormId(): string {
+    return 'ws_mercado_libre_mercado_libre_products';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, UserInterface $user = NULL): array {
+
+    $form['info'] = [
+      '#markup' => $this->t('Publicar en Mercado Libre mis productos que publique en este sitio'),
+    ];
+
+    $form['publicar'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Publicar productos'),
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Publicar'),
+      ],
+    ];
+
+    $form_state->set('user', $user);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    // @todo Validate the form here.
+    // Example:
+    // @code
+    //   if (mb_strlen($form_state->getValue('message')) < 10) {
+    //     $form_state->setErrorByName(
+    //       'message',
+    //       $this->t('Message should be at least 10 characters.'),
+    //     );
+    //   }
+    // @endcode
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $user = $form_state->get('user');
+
+    $config = $this->configFactory->get('ws_mercado_libre.settings');
+    $client_id = $config->get('client_id');
+    $client_secret = $config->get('client_secret');
+    $redirect_uri = $config->get('url_redirect');
+
+    \Drupal::messenger()->addMessage(t('client_id %client_id.', ['%client_id' => $client_id]));
+    
+    //If publish_products is checked, initiate OAuth flow.
+    if ($form_state->getValue('publicar')) {
+      $auth_url = "https://auth.mercadolibre.com.mx/authorization?response_type=code&client_id=$client_id&redirect_uri=" . urlencode($redirect_uri);
+      $response = new RedirectResponse($auth_url);
+      $response->send();
+      exit();
+    }
+
+    $this->messenger()->addStatus($this->t('Ahora los productos que publiques en este sitio tambien seran publicados en tu cuenta de Mercado libre.'));
+    $form_state->setRedirect('ws_mercado_libre.user', ['user' => $user->id()]);
+  }
+
+  // public function loguearUsuarioMl(){
+
+  //   $app_id = '';
+  //   $url_notify = '';
+  //   $code_chalenge = '';
+  //   $code_chalenge_method = '';
+  //   https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=$APP_ID&redirect_uri=$YOUR_URL&code_challenge=$CODE_CHALLENGE&code_challenge_method=$CODE_METHOD
+  
+  // }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
