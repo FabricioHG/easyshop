2fd5d4691d0c630ffeb8d198580df2f981c0e775
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2024-10-18 14:00:17 -0600

    Se ajusta campo expire date sumando tiempo actual 6 hrs, que es lo que dura el token

--- web/modules/custom/ws_mercado_libre/src/Service/UserMercadoLibre.php
+++ web/modules/custom/ws_mercado_libre/src/Service/UserMercadoLibre.php
@@ -125,7 +125,7 @@ class UserMercadoLibre
 	        $data = json_decode($body, TRUE);
 	        $access_token_new = $data['access_token'];
      		$refresh_token_new = $data['refresh_token'];
-     		$token_expires_in = $data['expires_in'];
+     		$token_expires_in = time() + 21600;
      		$username = $this->userEntity->getDisplayName();
 
 	      	// Save the tokens to the user's configuration or database.
