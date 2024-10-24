8c0f669a0b024ae1b754a584cc3f3ffc826b6fce
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2024-10-24 16:43:09 -0600

    se realizan cambios para publicacion de producto en mercado libre v16

--- web/modules/custom/ws_mercado_libre/src/Controller/WsMercadoLibreController.php
+++ web/modules/custom/ws_mercado_libre/src/Controller/WsMercadoLibreController.php
@@ -128,7 +128,7 @@ final class WsMercadoLibreController extends ControllerBase {
 
     $mercado_libre_service = \Drupal::service('ws_mercado_libre.mercadolibre_service');
 
-    $codigo_categoria = $mercado_libre_service->predecir_categoria('AOCHUAN estabilizador de mano de carga inalámbrica');
+    $codigo_categoria = $mercado_libre_service->predecir_categoria('Micrófono Lavalier inalámbrico portátil.');
     $atributos_obligatorios = $mercado_libre_service->obtener_attr_obligatorios($codigo_categoria);
     
     if (!empty($atributos_obligatorios) ) {
