014413404aecdbbb0cbb4de82bdac6f65caa92ea
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2025-03-04 17:05:45 -0600

    Se agregan modulos blog y aut con facebook y google 4

--- web/themes/custom/ws_ecommerce/js/ws-ecommerce.js
+++ web/themes/custom/ws_ecommerce/js/ws-ecommerce.js
@@ -48,22 +48,22 @@
       }
 
       /* Agregar SDK de login con facebook */
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '620096173968475',
-          xfbml      : true,
-          version    : 'v22.0'
-        });
-        FB.AppEvents.logPageView();
-      };
+      // window.fbAsyncInit = function() {
+      //   FB.init({
+      //     appId      : '620096173968475',
+      //     xfbml      : true,
+      //     version    : 'v22.0'
+      //   });
+      //   FB.AppEvents.logPageView();
+      // };
     
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "https://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
+      // (function(d, s, id){
+      //    var js, fjs = d.getElementsByTagName(s)[0];
+      //    if (d.getElementById(id)) {return;}
+      //    js = d.createElement(s); js.id = id;
+      //    js.src = "https://connect.facebook.net/en_US/sdk.js";
+      //    fjs.parentNode.insertBefore(js, fjs);
+      //  }(document, 'script', 'facebook-jssdk'));
 
       
     }
