13c6a2e71e043af9384c191c816bf3166e8baae2
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2025-03-03 18:03:06 -0600

    Se agregan modulos blog y aut con facebook y google 2

--- web/themes/custom/ws_ecommerce/js/ws-ecommerce.js
+++ web/themes/custom/ws_ecommerce/js/ws-ecommerce.js
@@ -50,23 +50,22 @@
       /* Agregar SDK de login con facebook */
       window.fbAsyncInit = function() {
         FB.init({
-          appId      : '{your-app-id}',
-          cookie     : true,
+          appId      : '1144476250262343',
           xfbml      : true,
-          version    : '{api-version}'
+          version    : 'v22.0'
         });
-          
-        FB.AppEvents.logPageView();   
-          
+        FB.AppEvents.logPageView();
       };
-
+    
       (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+      
     }
 
     
